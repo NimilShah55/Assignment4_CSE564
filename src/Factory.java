fbf95fa38f871591d1e434bc80458721e42cd60f
Nimil Shah <nimilshah030599@gmail.com>
2021-11-17 18:53:58 -0700

    Added javadocs

--- src/Factory.java
+++ src/Factory.java
@@ -1,6 +1,11 @@
 import javax.swing.text.AttributeSet.ColorAttribute;
 import java.awt.Color;
 
+
+/**
+ * Factory interface for creating an object.
+ * @author Nimil Shah
+ */
 public interface Factory {
 	public City createCity(String name, int cityX, int cityY, int size, Color colour, String type);
 }
--- src/ProductCity.java
+++ src/ProductCity.java
@@ -1,7 +1,11 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+
+/**
+ * Product city interface for creating an object.
+ * @author Nimil Shah
+ */
 public interface ProductCity {
 	public void draw(Graphics g);
-//	public void drawConnect(City b, Graphics2D g);
 }
